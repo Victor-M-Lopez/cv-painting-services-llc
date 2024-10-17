ecdcd594bcf37ebda31d57f6a9abeb415f1a228a
Victor Lopez <vic.m.lopez114@gmail.com>
2024-10-16 20:06:13 -0400

    added footer

--- apps.js
+++ apps.js
@@ -5,7 +5,6 @@ function insertEmail () {
   let emailSign = '&#64;';
   let emailExtension = '&#99;&#118;&#112;&#97;&#105;&#110;&#116;&#105;&#110;&#103;&#115;&#101;&#114;&#118;&#105;&#99;&#101;&#115;&#108;&#108;&#99;&#46;&#99;&#111;&#109;';
   emailSpace.innerHTML = `<a href=mailto:${emailName}${emailSign}${emailExtension} class="mail">${emailName}${emailSign}${emailExtension} </a>`;
-
 }
 
 window.onload = insertEmail;
