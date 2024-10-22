e1762aa0ffddf1d62c2cc114d811d4dcff52c8da
Victor Lopez <vic.m.lopez114@gmail.com>
2024-10-22 11:12:55 -0400

    added collapsable containers for special services

--- apps.js
+++ apps.js
@@ -9,9 +9,31 @@ function insertEmail () {
 
 window.onload = insertEmail;
 
-let specialService = document.querySelectorAll('.special_service');
-let plusIcon = document.querySelectorAll('plus_icon');
-let specialServiceText = document.querySelectorAll('.service_hidden');
+let specialService = document.querySelector('.special_service')
+let specialService2 = document.querySelector('.special_service-2')
+let specialService3 = document.querySelector('.special_service-3')
+let plusIcon = document.getElementById('plus_icon');
+let plusIcon2 = document.getElementById('plus_icon-2');
+let plusIcon3 = document.getElementById('plus_icon-3');
+let specialServiceText = document.querySelector('.service_hidden');
+let specialServiceText2 = document.querySelector('.service_hidden-2');
+let specialServiceText3 = document.querySelector('.service_hidden-3');
 
+specialService.addEventListener('click', function() {
+  plusIcon.classList.toggle('plus_icon-rotate');
+  specialService.classList.toggle('height_change');
+  specialServiceText.classList.toggle('text_appear');
+})
 
+specialService2.addEventListener('click', function() {
+  plusIcon2.classList.toggle('plus_icon-rotate');
+  specialService2.classList.toggle('height_change');
+  specialServiceText2.classList.toggle('text_appear');
+})
+
+specialService3.addEventListener('click', function() {
+  plusIcon3.classList.toggle('plus_icon-rotate');
+  specialService3.classList.toggle('height_change');
+  specialServiceText3.classList.toggle('text_appear');
+})
 
