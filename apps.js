f45b4d085ed0ec28502842ffda93e42bdfc21aa8
Victor Lopez <vic.m.lopez114@gmail.com>
2024-11-20 01:02:52 -0500

    added all alt tags and link urls

--- apps.js
+++ apps.js
@@ -1,10 +1,12 @@
-let emailSpace = document.getElementById('email');
+let emailSpace = document.querySelector('.email');
+let navEmail = document.querySelector('.nav_email');
 
 function insertEmail () {
   let emailName = '&#105;&#110;&#102;&#111;';
   let emailSign = '&#64;';
   let emailExtension = '&#99;&#118;&#112;&#97;&#105;&#110;&#116;&#105;&#110;&#103;&#115;&#101;&#114;&#118;&#105;&#99;&#101;&#115;&#108;&#108;&#99;&#46;&#99;&#111;&#109;';
   emailSpace.innerHTML = `<a href=mailto:${emailName}${emailSign}${emailExtension} class="mail">${emailName}${emailSign}${emailExtension} </a>`;
+  navEmail.setAttribute('href', `mailto:info@cvpaintingservicesllc.com`);
 }
 
 window.onload = insertEmail;
