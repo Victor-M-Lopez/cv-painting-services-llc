b37324dc080e39fe72679d24a7ba7b8f3d66b993
Victor Lopez <vic.m.lopez114@gmail.com>
2024-11-19 20:07:02 -0500

    added modal for navigation when on smaller phones

--- apps.js
+++ apps.js
@@ -43,13 +43,23 @@ for (let i = 0; i < specialService.length; i++) {
 let exitBtn = document.querySelector('.exit_btn');
 let modalBtn = document.querySelector('.nav_btn');
 
+let modal = document.getElementById('modal');
+let modalLink = document.querySelectorAll('.modal_link')
+
 modalBtn.addEventListener('click', function() {
   modalBtn.classList.add('btn_hidden');
   exitBtn.classList.add('btn_appear');
+  modal.classList.remove('hide_modal');
 });
 
-exitBtn.addEventListener('click', function() {
+exitBtn.addEventListener('click', modalHide);
+
+for (let i = 0; i < modalLink.length; i++) {
+  modalLink[i].addEventListener('click', modalHide);
+}
+
+function modalHide() {
   modalBtn.classList.remove('btn_hidden');
   exitBtn.classList.remove('btn_appear');
-  
-});
+  modal.classList.add('hide_modal');
+} 
